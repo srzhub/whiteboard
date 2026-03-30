1fb48239f5410a4f8ca2d7584e4f3ec3a874cfd2
suhaib <suhaib.aazmi05@gmail.com>
2026-03-30 19:16:27 +0530

    Added user colors, eraser,  multi-room logic

--- server.js
+++ server.js
@@ -4,30 +4,56 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
 // This array stores every draw event since the server started.
-// SDE Note: In production, use Redis or a Database to avoid RAM overflow.
-let history = []; 
+// In production, use Redis or a Database to avoid RAM overflow.
+let roomData = {}; 
 
+// Serve (route) the static HTML file when anyone acceses root URL
 app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
 
+let userCount = 0;
+// Fires every time a new user connects to the server via Socket.IO
 io.on('connection', (socket) => {
-    console.log('A user connected. Current history size:', history.length);
 
-    // 1. Immediate Sync: Send the current state to the user who just joined
-    socket.emit('history', history);
+    userCount++;
+    io.emit('userCount', userCount) // brodcast number of users to all
+    console.log('A user connected. Current number of rooms:', Object.keys(roomData).length);
+
+    socket.on('join', (roomName) => {
+        socket.join(roomName);
+        socket.currentRoom = roomName;
+
+        if(!roomData[roomName]){
+            roomData[roomName] = [];
+        }
+
+        // send only this rooms history to user
+        socket.emit('history', roomData[roomName]);
+        console.log(`User Joined Room: ${roomName}`)
+    });
 
     socket.on('draw', (data) => {
-        // 2. Save event to history so future joiners see it
-        history.push(data); 
-        // 3. Broadcast to all other active users
-        socket.broadcast.emit('draw', data);
+        const room = socket.currentRoom;
+        if(room){
+            roomData[room].push(data);
+            // Brodcast only to people in this room other than drawer himself
+            socket.to(room).emit('draw', data);
+        }
+        else{
+            console.log("REJECTED: Drawing received but socket is not in a room yet.");
+        }
     });
 
     socket.on('clear', () => {
-        history = []; // Wipe the server-side history
-        io.emit('clear'); // Tell EVERYONE (including sender) to clear screens
+        const room = socket.currentRoom;
+            if (room) {
+                roomData[room] = [];
+                io.to(room).emit('clear'); // Tell everyone in the room to clear
+            }
     });
 
     socket.on('disconnect', () => {
+        userCount--;
+        io.emit('userCount', userCount);
         console.log('User disconnected');
     });
 });
